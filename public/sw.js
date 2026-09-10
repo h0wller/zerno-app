9f10ddf7fa390df945d7088e59d8e966fa29c014
h0wller <woxyara76@gmail.com>
2026-09-11 01:16:11 +0200

    half fix

--- public/sw.js
+++ public/sw.js
@@ -24,7 +24,7 @@ self.addEventListener('fetch', e => {
 self.addEventListener('push', e => {
   let data = {};
   try { data = e.data.json(); } catch (_) {}
-    self.clients.matchAll({ type: 'windows', includeUncontrolled: true })
+    self.clients.matchAll({ type: 'window', includeUncontrolled: true })
     .then(cs => cs.forEach(c => c.postMessage({ type: 'zpush', title: (data && data.title) || '' })));
   e.waitUntil(self.registration.showNotification(data.title || '…и кофе', {
     body: data.body || '☕',
