aac79332147b27cf7f1dd8f38e1dd2e358eac4e6
h0wller <woxyara76@gmail.com>
2026-09-13 22:41:26 +0200

    refactor(frontend): extract cashier to public/app/cashier.js (F2.3)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zerno-v170';
+const CACHE = 'zerno-v171 ';
 const PRECACHE = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', e => {
