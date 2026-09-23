f2f983db029732351e8622d7aeeaa64645e8dd09
Alexandr Sysoev <38438013+h0wller@users.noreply.github.com>
2026-09-23 09:22:32 +0200

    Update sw.js
    Рефакторинг scrolltop с новой логикой

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // public/sw.js
-const STATIC_CACHE = 'zerno-static-v68'; // ← поставь своё текущее значение +1
+const STATIC_CACHE = 'zerno-static-v69'; // ← поставь своё текущее значение +1
 const MEDIA_CACHE = 'zerno-media-v10';
 const API_CACHE = 'zerno-api-v6';
 
