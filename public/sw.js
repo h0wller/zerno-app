d28123c3086957bd3c90c8d6929bc9d678588228
Alexandr Sysoev <38438013+h0wller@users.noreply.github.com>
2026-09-23 10:37:15 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // public/sw.js
-const STATIC_CACHE = 'zerno-static-v70'; // ← поставь своё текущее значение +1
+const STATIC_CACHE = 'zerno-static-v71'; // ← поставь своё текущее значение +1
 const MEDIA_CACHE = 'zerno-media-v10';
 const API_CACHE = 'zerno-api-v6';
 
